727b62b000b19a80cd2f629401a0b29a2416cddd
Eser Ozvataf <eser@ozvataf.com>
2021-10-18 02:45:55 +0300

    typo fixes.

--- src/app/pages/cv/index.tsx
+++ src/app/pages/cv/index.tsx
@@ -14,24 +14,26 @@ function Cv() {
 
 Nov 2020 - Present
 
-An IT project delivery and outsource contractor initiative which provides engineering know-how and work force for its customers.
+Açık Yazılım is founded by myself during the COVID-19 pandemic. As a software consulting company, it outsources know-how and workforce for its customers contracted with.
 
 Achievements:
-- Delivered and maintained a contracted work which is a marketplace specialized on holistic wellbeing services
+- Founded the company and contracted with customers
+- Delivered and maintained a contracted work which is a marketplace specialized in holistic wellbeing services
 - Provided consultancy to a well-known Turkish Unicorn during their transition to product-based agile teams
 
-Technologies, Platforms and Concepts experienced during the period:
+Technologies, Platforms, and Concepts experienced during the period:
 - JavaScript, Python
-- Amazon Web Services (AWS), Google Cloud Provider (GCP), Cloudflare
-- Kubernetes, Docker, AWS ECS, GCP GKE Autopilot, Cloudflare Pages
-- React, React Hooks, Graphene GraphQL, Django
+- Amazon Web Services, Google Cloud Provider, Cloudflare, Cloudflare Pages, GitHub Actions
+- Kubernetes, Docker
+- Node.js, Deno, Cypress, Jest, webpack
+- React, React Hooks, GraphQL, Django
 
 
 ### Coordinator of Projects, Turkish Open Source Platform
 
 Dec 2020 - May 2021
 
-An establishment under the auspices of the Ministry of Industry and Technology of the Turkish state, Turkey Open Source Platform aims to increase the number of software developers Turkey needs, to increase the qualifications of existing software developers and to develop the open source ecosystem. In this respect, the platform performs a similar function to CNCF (CloudNative Foundation) and Linux Foundation.
+An establishment under the auspices of the Ministry of Industry and Technology of the Turkish state, Turkey Open Source Platform aims to increase the number of software developers Turkey needs, to increase the qualifications of existing software developers, and to develop the open-source ecosystem. In this respect, the platform performs a similar function to CNCF (CloudNative Foundation) and Linux Foundation.
 
 Achievements:
 - Established and facilitated weekly meetings of project groups under five different project topics in order to produce the software needed by the public and private sectors in Turkey with open source software (These topics were Natural Language Processing, Cloud and Database Systems, Mobility, Linux (Pardus) and Digital Badges.)
@@ -48,23 +50,25 @@ Sep 2018 - Nov 2020
 Setur is the tourism and duty-free merchandising company of Koç Holding Group of Companies, which is on the Fortune 500 list.
 
 Achievements:
-- Led and coached the management during Agile and DevOps transformation
-- Redesigned software and product development processes
-- Formed and facilitated 9 agile product teams, led 30+ people with various functions (UX, QA, SE, BA etc.)
-- Designed company-wide prioritization flow and how product teams function in daily basis. Maintained source and delivery planning of product teams
-- Transformed Windows & .NET only ecosystem into polyglot-enabled, linux-containerized, orchestrated and multi-regional infrastructure runs on Kubernetes
-- Established software quality assurance and delivery metrics with monitoring, tracing and alerting mechanisms
-- Improved IT recruitment processes, attended 300+ recruitment meetings
+- Accomplished the leadership and coaching of Agile & DevOps transformation which led the company to have 80% accurate estimations
+- Led 9 cross-functional product teams, with a total of 30+ people with various functions (UX, QA, SE, BA, etc.)
+- Designed company-wide prioritization flow to maintain both product and technical roadmaps of products
+- Achieved faster onboarding process and development of culture by initiating a knowledge base that consists of both cultural and technical guidelines
+- Achieved faster troubleshooting and bug detection by established software quality assurance and delivery metrics with monitoring, tracing, and alerting mechanisms
+- Transformed Windows & .NET only ecosystem into polyglot-enabled, Linux-containerized, orchestrated and multi-regional infrastructure runs on Kubernetes
+- Improved IT recruitment processes, implemented technical assessments, attended 300+ recruitment meetings
 - Enabled remote and multi-regional development culture
-- Organized meetup and training sessions
+- Organized a meetup and several training sessions
 
-Technologies, Platforms and Concepts experienced during the period:
+Technologies, Platforms, and Concepts experienced during the period:
 - JavaScript, .NET, Java
-- Microsoft Azure
-- Kubernetes, Docker, Azure AKS, Azure AppServices
-- Linux, Windows, macOS - React, React Native
-- node.js, Postgres, SQL Server
-- Microservices, Domain Driven Design (DDD)
+- Microsoft Azure, Azure DevOps
+- Linux, Windows, macOS
+- Kubernetes, Docker
+- Postgres, SQL Server, MongoDB, ElasticSearch
+- Node.js, Cypress, Jest, webpack, Kibana, Grafana
+- React, React Native
+- Microservices, DDD
 - CloudNative, OpenSource
 
 
@@ -75,24 +79,40 @@ May 2016 - Sep 2018
 KoçSistem is the information technology company of Koç Holding Group of Companies, which is on the Fortune 500 list. They offers a wide range of products and services in areas such as Internet of Things, Big Data & Analytics, Security, Corporate Cloud, Corporate Mobility, Business Solutions, Digital Workforce and RPA.
 
 Achievements:
-- Kicked off company's main application framework and was first member of the company's DevOps and Software Architecture teams
-- Helped other teams to embrace Agile and DevOps practices,
+- Kicked off the company's main application framework and was the first member of the company's DevOps and Software Architecture teams
+- Developed a React project for initializing codebases that use the company's main application framework with selected components on a web wizard form
+- Helped other teams to embrace Agile and DevOps practices
 - Took parts of analysis and decisions of company's product choices (such as CMSes, orchestration tools, etc)
-- Delivered company's first serverless project runs on AWS Lambda
+- Delivered the company's first serverless project that runs on AWS Lambda
 - Delivered and maintained 3 enterprise solutions to customers
 - Attended recruitment meetings as a technical interviewer
 
-Technologies, Platforms and Concepts experienced during the period:
+Technologies, Platforms, and Concepts experienced during the period:
 - JavaScript, .NET
-- Amazon Web Services (AWS)
-- Docker, MongoDB
-- node.js, AWS S3, AWS Lambda, AWS Cognito
-- FaaS, Domain Driven Design (DDD)
+- Amazon Web Services
+- Jenkins, JMeter, Docker
+- SQL Server, Oracle, MongoDB, ElasticSearch
+- Node.js, AWS Lambda, AWS Cognito, AWS S3
+- React, Redux
+- FaaS, DDD
 
 
 ### Software Architect, Mobilex
 
 Sep 2015 - Apr 2016
+
+MobileX is a fast-growing tech company focused on building and marketing highly scalable mobile applications. Despite being such a young company, they have a track record of building & growing mobile apps on various verticals that reached over 120 million users across 160 countries.
+
+Achievements:
+- Developed a complete backend and cloud architecture for Instagram-like application with location-awareness
+
+Technologies, Platforms, and Concepts experienced during the period:
+- JavaScript
+- Amazon Web Services
+- Terraform, Docker
+- Postgres, MongoDB
+- Node.js, AWS VPC, AWS EC2, AWS ElasticBeanstalk, AWS S3
+- OpenStreetMaps
 
 
 ### E-Commerce Software Consultant, Zaimoglu Holding
